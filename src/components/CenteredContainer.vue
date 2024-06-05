cb403095eba34e072423c5b307228c74f44b3dcd
anamboijohn <anamboijohn@gmail.com>
2024-06-05 07:20:35 +0000

    test pages

---
+++ src/components/CenteredContainer.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="q-mr-auto q-ml-auto" style="max-width: 700px"></div>
+  <slot></slot>
+</template>
+
+<script setup lang="ts"></script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,21 @@
 <template>
-  Name: <input type="text" name="name" v-model="name" placeholder="Enter your name" />
+  <q-page class="flex flex-center">
+    <q-card style="max-width: 500px;">
+     <q-card-section horizontal>
+       <q-image>
+          <img src="https://b" alt="My name">
+       </q-image>
+       <q-card-section>
+        {{name}} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam placeat molestias doloribus reprehenderit tempora enim praesentium consectetur deleniti sunt reiciendis accusantium atque beatae eligendi, ratione quos officiis aut labore ad.
+       </q-card-section>
+     </q-card-section>
+    </q-card>
+  </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-let name = ref("");
+import { ref } from 'vue';
+//import centeredContainer from '../components/CenteredContainer.vue';
+
+let name = ref('My name');
 </script>
