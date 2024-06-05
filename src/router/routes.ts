7c991507c987b8013fea44cc88f894fbc25ea939
anamboijohn <anamboijohn@gmail.com>
2024-06-05 07:19:29 +0000

    add signin and signup routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,11 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'signup', component: () => import('pages/signUpPage.vue') },
+      { path: 'signin', component: () => import('pages/signInPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
