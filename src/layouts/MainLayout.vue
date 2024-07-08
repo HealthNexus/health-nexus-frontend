3aacd4cb621047ac9b126e00146c3f7454a59151
anamboijohn <anamboijohn@gmail.com>
2024-07-08 23:24:50 +0000

    change layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,28 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header elevated class="bg-primary text-white" height-hint="98">
+    <q-header elevated class="bg-primary text-white">
       <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
         <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
           HealthNexus
         </q-toolbar-title>
+        <q-tabs align="left">
+          <q-route-tab :to="{name:'signin'}" label="sign in" />
+          <q-route-tab :to="{name:'signup'}" label="sign up" />
+          <q-route-tab :to="{name:'posts'}" label="posts" />
+        </q-tabs>
       </q-toolbar>
-
-      <q-tabs align="left">
-        <q-route-tab :to="{name:'signin'}" label="sign in" />
-        <q-route-tab :to="{name:'signup'}" label="sign up" />
-        <q-route-tab :to="{name:'posts'}" label="posts" />
-      </q-tabs>
     </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <!-- drawer content -->
+       
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -21,3 +30,20 @@
 
   </q-layout>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
