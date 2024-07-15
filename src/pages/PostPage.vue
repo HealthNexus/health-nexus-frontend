b129f2290a9e4c6cc7ac200c825f808127ad994a
anamboijohn <anamboijohn@gmail.com>
2024-07-15 23:56:10 +0000

    add message box

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -19,6 +19,43 @@
         <div>{{ postStore.post?.body }}</div>
       </div>
     </div>
+    <div class="q-pa-md row justify-end">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+          :text="['Have you seen Quasar?']"
+          sent
+          text-color="white"
+          bg-color="primary"
+        >
+          <template v-slot:name>me</template>
+          <template v-slot:stamp>7 minutes ago</template>
+          <template v-slot:avatar>
+            <img
+              class="q-message-avatar q-message-avatar--sent"
+              src="https://cdn.quasar.dev/img/avatar4.jpg"
+            >
+          </template>
+        </q-chat-message>
+
+        <q-chat-message
+          bg-color="amber"
+        >
+          <template v-slot:name>Mary</template>
+          <template v-slot:avatar>
+            <img
+              class="q-message-avatar q-message-avatar--received"
+              src="https://cdn.quasar.dev/img/avatar2.jpg"
+            >
+          </template>
+
+          <div>
+            Already building an app with it...
+          </div>
+
+          <q-spinner-dots size="2rem" />
+        </q-chat-message>
+      </div>
+    </div>
   </q-page>
 </template>
 
