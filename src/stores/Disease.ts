a1b251f5b9066604e2debb22a17b05d76df24283
anamboijohn <anamboijohn@gmail.com>
2024-07-29 12:45:35 +0000

    define functions to fetch symptoms and drugs

--- src/stores/Disease.ts
+++ src/stores/Disease.ts
@@ -5,19 +5,29 @@ import { ref} from 'vue';
 import axios from 'axios';
 
 
-interface Disease{
+export interface Disease{
   id: 1,
   name: string,
   slug: string
 }
 
 
+export interface Symptom{
+  id: number;
+  description: string;
+}
 
-
+export interface Drug{
+  id: number;
+  name: string;
+  slug: string;
+}
   export const useDiseaseStore = defineStore('disease', () => {
 
   const API_URL = 'http://localhost:8000/api';
   const diseases: Ref<Disease[]> = ref([]);
+  const symptoms: Ref<Symptom[]> = ref([]);
+  const drugs: Ref<Drug[]> = ref([]);
 
   const fetchDiseases = async () => {
     try{
@@ -29,12 +39,47 @@ interface Disease{
     }
   }
 
+  const fetchSymptoms = async () => {
+    try{
+      const token = localStorage.getItem('auth_token');
+      if (!token) throw new Error('No token found');
+      const response = await axios.get(`${API_URL}/symptoms`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      });
+      symptoms.value = response.data.symptoms;
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  const fetchDrugs = async () => {
+    try{
+      const token = localStorage.getItem('auth_token');
+      if (!token) throw new Error('No token found');
+      const response = await axios.get(`${API_URL}/drugs`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+      });
+      drugs.value = response.data.drugs;
+    }catch(error){
+      console.log(error)
+    }
+  }
+
 
 
 
 
   return {
-    fetchDiseases
+    fetchDiseases,
+    fetchSymptoms,
+    diseases,
+    symptoms,
+    drugs,
+    fetchDrugs
   };
 });
 
