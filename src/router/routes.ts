70d7721556b0de077da5af7348a61ff2432b128b
NNanakwame <n4nanakwame@gmail.com>
2024-08-23 10:32:24 +0000

    sync

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -20,7 +20,9 @@ const routes: RouteRecordRaw[] = [
       { path: 'test', name:'test', component: () => import('pages/TestPage.vue') },
       { path: 'records', name:'records', component: () => import('pages/RecordsPage.vue') },
       { path: 'landing', name:'Landing', component: () => import('src/pages/LandingPage.vue') },
+      { path: 'blogs', name:'Blog', component: () => import('src/pages/BlogTest.vue') },
       { path: 'record/create', name:'record.create', component: () => import('src/pages/CreateRecordPage.vue') },
+
 
       // Posts routes
       { path: 'posts/', component: () => import('pages/IndexPage.vue'), name:'posts' },
